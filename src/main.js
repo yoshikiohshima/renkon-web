7d8086205996f4f244e139310a886ce7be06df2e
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2025-07-25 16:31:17 -0700

    use 0.8.11. export more things from various CodeMirror packages

--- src/main.js
+++ src/main.js
@@ -3,14 +3,23 @@ export {ProgramState, parseJSX, transpileJSX, translateTS, globals, version} fro
 export {view} from "./system";
 export {newInspector} from "./inspector";
 import * as C from "codemirror";
-import {keymap, hoverTooltip} from "@codemirror/view";
-import {indentWithTab} from "@codemirror/commands";
-import {javascript, esLint} from "@codemirror/lang-javascript";
-import {linter, lintGutter} from "@codemirror/lint";
-import {SearchQuery} from "@codemirror/search";
+import * as V from "@codemirror/view";
+import * as Com from "@codemirror/commands";
+import * as Lang_J from "@codemirror/lang-javascript";
+import * as Lint from "@codemirror/lint";
+import * as Search from "@codemirror/search";
 
 import * as eslint from "eslint-linter-browserify";
 
-export const CodeMirror = {...C, keymap, indentWithTab, javascript, linter, lintGutter, esLint, eslint, hoverTooltip, search, SearchQuery, globals};
+export const CodeMirror = {
+    ...C,
+    view: V,
+    commands: Com,
+    "lang-javascript": Lang_J,
+    lint: Lint,
+    search: Search,
+    "eslint-linter-browserify": eslint,
+    globals
+};
 
 console.log("Renkon version:" + version);
